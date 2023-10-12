2b8591b2074a0bc02003b366ec0c28dd0948975a
Felix11coder <felixkngugi@gmail.com>
2023-10-12 08:03:46 +0300

    initial commit

---
+++ djangoProject/toshibaPurePython/input/out[ut.py
@@ -0,0 +1,7 @@
+#user input
+jina=input('Enter your name : ')
+print(f'Welcome {jina}')
+
+num=int(input("Enter number : "))
+print(f'You entered number {num}')
+print(f'The datatype is {type(num)}')
